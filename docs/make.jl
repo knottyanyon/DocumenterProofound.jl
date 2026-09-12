2f16c45e9537dfa9843675d40b7385aea46028b5
knottyanyon <62780345+knottyanyon@users.noreply.github.com>
2026-09-12 14:10:32 +0200

    docs: add kitchen sink page demonstrating the theme, apply own theme to own docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,11 +1,25 @@
 using Documenter
 using DocumenterProofound
 
+src_dir = joinpath(@__DIR__, "src")
+
+# Vendor this package's own compiled theme CSS/JS into docs/src/assets/proofound/ -
+# Documenter only serves files under docs/src/assets/.
+let assets_src = DocumenterProofound.assets_dir(), assets_dst = joinpath(src_dir, "assets", "proofound")
+    isdir(assets_dst) && rm(assets_dst; recursive=true)
+    cp(assets_src, assets_dst)
+end
+
+DocumenterProofound.setup(src_dir)
+
 makedocs(;
     modules=[DocumenterProofound],
     authors="knottyanyon",
     sitename="DocumenterProofound.jl",
     repo="https://github.com/knottyanyon/DocumenterProofound.jl/blob/{commit}{path}#{line}",
-    format=Documenter.HTML(; edit_link="main"),
-    pages=["Home" => "index.md"],
+    format=Documenter.HTML(;
+        edit_link="main",
+        assets=joinpath.("assets/proofound", DocumenterProofound.assets()),
+    ),
+    pages=["Home" => "index.md", "Kitchen Sink" => "kitchen_sink.md"],
 )
